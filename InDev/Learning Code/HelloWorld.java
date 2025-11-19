257c5bd8ceb5d22e5930e7d5d298bd6893e8d168
WillDone-MT <wildanmufti350@gmail.com>
2025-11-19 10:13:08 +0700

    Security [2] kinda, because the files that were in InDev before are even not an InDev

--- InDev/Learning Code/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-// A simple Java program to print "Hello, World!"
-public class HelloWorld {
-    public static void main(String[] args) {
-        // Print message to the console
-        System.out.println("Hello, World!");
-    }
-}
